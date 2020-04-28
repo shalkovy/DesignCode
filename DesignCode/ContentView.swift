82629610894985e2285dbf5bbcdb3415d0240366
Dima Shelkov <shalkovy@gmail.com>
2020-04-28 20:52:09 +0300

    feat: add more views

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,28 +9,115 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            VStack {
-                VStack {
-                    Text("UI Design")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color("accent"))
-                        .padding(.top)
-                    Text("Certificate")
-                        .foregroundColor(.white)
-                }
-            }
-            Image("Background")
+        ZStack {
+            
+            TitleView()
+                 .blur(radius: 20)
+            
+            CardBottomView()
+                .blur(radius: 20)
+            
+            CardView()
+                .offset(x: 0, y: -40)
+                .scaleEffect(0.85)
+                .rotationEffect(Angle(degrees: 15))
+                .rotation3DEffect(Angle(degrees: 50), axis: (x: 10, y: 10, z: 10))
+                .blendMode(.hardLight)
+            
+            CardView()
+                .offset(x: 0, y: -20)
+                .scaleEffect(0.9)
+                .rotationEffect(Angle(degrees: 10))
+                .rotation3DEffect(Angle(degrees: 40), axis: (x: 10, y: 10, z: 10))
+                .blendMode(.hardLight)
+            
+            CretificateView()
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: 5))
+                .rotation3DEffect(Angle(degrees: 30), axis: (x: 10, y: 10, z: 10))
         }
-        .background(Color.black)
-        .cornerRadius(10)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            Text("Card Back")
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.blue)
+        .cornerRadius(10)
+        .shadow(radius: 20)
+    }
+}
+
+struct CretificateView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("accent"))
+                        .padding(.top)
+                    Text("Certificate")
+                        .foregroundColor(.white)
+                }
+                Spacer()
+                Image("Logo").resizable()
+                    .frame(width: 30.0, height: 30.0)
+            }
+            .padding(.horizontal)
+            Spacer()
+            Image("Background")
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(10)
+        .shadow(radius: 20)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Spacer()
+            }
+            Image("Illustration5")
+            Spacer()
+        }.padding()
+    }
+}
+
+struct CardBottomView: View {
+    var body: some View {
+        VStack(spacing: 20.0) {
+            Rectangle()
+                .frame(width: 60, height: 6)
+                .cornerRadius(3)
+                .opacity(0.1)
+            Text("asdadasd asdasd das asdsa asdasd asdasda da dsadas")
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .padding()
+        .padding(.horizontal)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(y: 600)
     }
 }
